06e7ab453e9eb75d57fae789518f832b50c8d476
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-10-15 00:06:29 +0200

    try to read several gifs

--- js/gif.js
+++ js/gif.js
@@ -1,30 +1,33 @@
-  var camera, scene, renderer, container;
-  var light, pointLight, geometry, mesh;
-  var uniforms, material;
-  var heightmap, diffTexture, dispTexture;
+  let camera, scene, renderer, container;
+  let light, pointLight, geometry, mesh;
+  let uniforms, material;
+  let heightmap, diffTexture, dispTexture;
 
   function start() {
     container = document.getElementById( 'container' );
-    var sup2 = new SuperGif({ gif: document.getElementById('example2') } );
-    sup2.load();
-    llamacanvas = sup2.get_canvas();
+    gifs = [];
+    gifItems = document.getElementsByClassName('gifClass');
+    //load gifs from the dom
+    for(let i = 0; i < gifItems.length ; i++) {
+      gifs[i] = new SuperGif({ gif: gifItems[i] } );
+    }
 
     // --- WebGl render
     try {
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.autoClear = false;
-        container.appendChild( renderer.domElement );
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.autoClear = false;
+      container.appendChild( renderer.domElement );
     }
     catch (e) {
-        alert(e);
+       alert(e);
     }
     scene = new THREE.Scene();
-    // --- Camera
-    var fov = 15; // camera field-of-view in degrees
-    var width = renderer.domElement.width;
-    var height = renderer.domElement.height;
-    var aspect = width / height; // view aspect ratio
+
+    const fov = 15;
+    const width = renderer.domElement.width;
+    const height = renderer.domElement.height;
+    const aspect = width / height; // view aspect ratio
     camera = new THREE.PerspectiveCamera( fov, aspect );
     camera.position.z = -600;
     camera.position.y = -800;
@@ -34,30 +37,38 @@
     controls.enableDamping = true;
     controls.dampingFactor =  0.25;
     controls.enableZoom = true;
-    controls.enablePan = false;
+    controls.enablePan = true;
     controls.enableKeys = false;
     controls.rotateSpeed = 3.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
-    controls.noZoom = false;
-    controls.noPan = false;
     controls.staticMoving = true;
     controls.dynamicDampingFactor = 0.3;
     controls.addEventListener( 'change', render );
-    // --- Lights
 
+    // --- Lights
     pointLight = new THREE.PointLight( 0xffffff, 1.0 );
     scene.add( pointLight );
-    pointLight.position.set(0, 100, -400);
-    // MATERIAL
-    material = new THREE.MeshStandardMaterial();
-    material.map = new THREE.Texture( llamacanvas );
-    material.displacementMap = material.map;
-    // GEOMETRY
-    geometry = new THREE.PlaneGeometry(256, 200, 256, 200);
-    mesh = new THREE.Mesh( geometry, material);
-    mesh.rotation.y = Math.PI;
-    scene.add(mesh);
+    pointLight.position.set(0, 100, -800);
+
+    console.log(gifs.length)
+    for(let i = 0; i < 1; i++) {
+      console.log(gifs[i])
+      gifs[i].load();
+      const gifcanvas = gifs[i].get_canvas();
+      // MATERIAL
+      material = new THREE.MeshStandardMaterial();
+      material.map = new THREE.Texture( gifcanvas );
+      material.displacementMap = material.map;
+      // GEOMETRY
+      geometry = new THREE.PlaneGeometry(64, 50, 64, 50);
+      mesh = new THREE.Mesh( geometry, material);
+      mesh.rotation.y = Math.PI;
+      mesh.position.x = 0 + (100 * i);
+      //mesh.position.y = positions[i].y;
+      //mesh.position.z = positions[i].z;
+      scene.add(mesh);
+    }
 
     setInterval("update()", 30);
     update();
