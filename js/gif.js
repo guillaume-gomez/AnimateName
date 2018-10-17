647b6391bf5aa0e07abffcf63fe2bef5e5d8cae9
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-10-18 00:32:37 +0200

    deploy

---
+++ js/gif.js
@@ -0,0 +1,115 @@
+  let camera, scene, renderer, container;
+  let light, pointLight;
+  let materials = [];
+
+  function createImgTags() {
+    const previewContainer = document.getElementById('previewContainer');
+    const urls = [
+      "https://media.giphy.com/media/nXxOjZrbnbRxS/giphy.gif",
+      "https://media.giphy.com/media/TaBMIY5wKBvaw/giphy.gif",
+      "https://media.giphy.com/media/12XMGIWtrHBl5e/giphy.gif",
+      "https://media.giphy.com/media/xUySTxVTWeEtJhEzsY/giphy.gif"
+    ];
+    for(let i = 0; i < urls.length; i++) {
+      createImgTag(previewContainer, `gif${i+1}`, urls[i]);
+    }
+  }
+
+  function createImgTag(parent, id, url, className = "gifClass") {
+    let img = document.createElement('img');
+    img.src = url;
+    img.id = id;
+    img.className = className;
+   // img.rel['rubbable'] = '1';
+   // img.rel['auto_play'] = '1';
+    parent.appendChild(img);
+  }
+
+  function start() {
+    container = document.getElementById( 'container' );
+    gifs = [];
+    gifItems = document.getElementsByClassName('gifClass');
+    //load gifs from the dom
+    for(let i = 0; i < gifItems.length ; i++) {
+      gifs[i] = new SuperGif({ gif: gifItems[i] } );
+    }
+
+    // --- WebGl render
+    try {
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.autoClear = false;
+      container.appendChild( renderer.domElement );
+    }
+    catch (e) {
+       alert(e);
+    }
+    scene = new THREE.Scene();
+
+    const fov = 15;
+    const width = renderer.domElement.width;
+    const height = renderer.domElement.height;
+    const aspect = width / height; // view aspect ratio
+    camera = new THREE.PerspectiveCamera( fov, aspect );
+    camera.position.z = -600;
+    camera.position.y = -800;
+    camera.lookAt(scene.position);
+    camera.updateMatrix();
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor =  0.25;
+    controls.enableZoom = true;
+    controls.enablePan = true;
+    controls.enableKeys = false;
+    controls.rotateSpeed = 3.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    controls.addEventListener( 'change', render );
+
+    // --- Lights
+    pointLight = new THREE.PointLight( 0xffffff, 1.0 );
+    scene.add( pointLight );
+    pointLight.position.set(0, 100, -800);
+
+    console.log(gifs.length)
+    for(let i = 0; i < 1; i++) {
+      gifs[i].load();
+      const gifcanvas = gifs[i].get_canvas();
+      // MATERIAL
+      material = new THREE.MeshStandardMaterial();
+      material.map = new THREE.Texture( gifcanvas );
+      material.displacementMap = material.map;
+      // GEOMETRY
+      geometry = new THREE.PlaneGeometry(128, 100, 128, 100);
+      mesh = new THREE.Mesh( geometry, material);
+      mesh.rotation.y = Math.PI;
+      mesh.position.x = 0 + (100 * i);
+      //mesh.position.y = positions[i].y;
+      //mesh.position.z = positions[i].z;
+      scene.add(mesh);
+    }
+
+    setInterval("update()", 30);
+    update();
+  }
+
+  function update() {
+    material.map.needsUpdate = true;
+    render();
+    controls.update(); // trackball interaction
+  }
+  function render() {
+    renderer.clear();
+    renderer.render(scene, camera);
+  }
+
+  window.onload = function() {
+    createImgTags();
+    const loadFunction = () => {
+      start();
+      controls.update();
+    }
+    setTimeout(loadFunction, 5000);
+  }
