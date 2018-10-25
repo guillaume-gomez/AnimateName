221d62dcb625035e672c56f81f1accc2c5cd314c
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-10-26 00:07:37 +0200

    split url differently

--- js/converter.js
+++ js/converter.js
@@ -1,13 +1,16 @@
-function submit(redirection) {
-    var textarea = document.getElementById("textarea");
-    var result = convertToJsonParam(textarea.value);
-    debugger
-    console.log(result)
-    var encodedResult = encodeURI(JSON.stringify(result))
-    window.location.replace(`${redirection}?text=${btoa(encodedResult)}`);
+function submit(redirection, hasUrl) {
+  var textarea = document.getElementById("textarea");
+  var result = convertToJsonParam(textarea.value, hasUrl);
+  var encodedResult = encodeURI(JSON.stringify(result));
+  window.location.replace(`${redirection}?text=${btoa(encodedResult)}`);
 }
 
-function convertToJsonParam(text) {
-    var data = text.replace('\t', '').replace('.', '\n').split('\n');
-    return { data };
+function convertToJsonParam(text, hasUrl) {
+  let data = text.replace('\t', '');
+  if(hasUrl) {
+    data = data.split('\n');
+  } else {
+    data = data.replace('.', '\n').split('\n');
+  }
+  return { data };
 }
