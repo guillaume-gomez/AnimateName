a166f6069a1c6e0519a6600d39992fca57bb1ec1
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-11-24 08:58:27 +0100

    use cache to avoid to load several times the same gif

--- js/gif.js
+++ js/gif.js
@@ -1,17 +1,18 @@
   let camera, scene, renderer, container;
   let light, pointLight;
+  let controls = null;
   let materials = [];
   const WIDTH = 128;
   const HEIGHT = 128;
   const OFFSET = 5;
-  let gifsCache = {};
-  
+
   function createImgTags() {
     const previewContainer = document.getElementById('previewContainer');
     //get the gif urls from the url params
     const url = new URL(window.location);
     const convertedParams = atob(url.searchParams.get("text"));
     const urls = JSON.parse(decodeURI(convertedParams)).data || ["https://media.giphy.com/media/nXxOjZrbnbRxS/giphy.gif"];
+    console.log(urls)
     for(let i = 0; i < urls.length; i++) {
       createImgTag(previewContainer, `gif${i+1}`, urls[i]);
     }
@@ -25,20 +26,45 @@
     img.width = WIDTH;
     img.height = HEIGHT;
     img.className = className;
-   // img.rel['rubbable'] = '1';
-   // img.rel['auto_play'] = '1';
     parent.appendChild(img);
   }
 
-  function start() {
+  function init() {
     container = document.getElementById( 'container' );
-    gifs = [];
-    gifItems = document.getElementsByClassName('gifClass');
+    let gifs = [];
+    let gifsCache = {};
+    let promises = [];
+    const gifItems = document.getElementsByClassName('gifClass');
     //load gifs from the dom
     for(let i = 0; i < gifItems.length ; i++) {
-      gifs[i] = new SuperGif({ gif: gifItems[i] } );
+      const url = gifItems[i].src;
+      let gif = null;
+      if(!gifsCache[url]) {
+        gif = new SuperGif({ gif: gifItems[i] } );
+        gifsCache[url] = { data: gif, indexes: [i] }
+      } else {
+        gif = gifsCache[url].data;
+        gifsCache[url].indexes.push(i)
+      }
+      gifs[i] = gif;
     }
-    // --- WebGl render
+
+    Object.entries(gifsCache).forEach(([key, values]) => {
+      const gifLoadedPromised = new Promise(function(resolve, reject) {
+          values.data.load(() => { 
+            resolve("ok");
+          });
+        });
+        promises.push(gifLoadedPromised);
+    });
+    
+    Promise.all(promises).then(function(values) {
+      start(gifs, gifsCache);
+    });
+  }
+
+  function initScene() {
+     // WebGl render
     try {
       renderer = new THREE.WebGLRenderer();
       renderer.setSize( window.innerWidth, window.innerHeight );
@@ -76,27 +102,28 @@
     pointLight = new THREE.PointLight( 0xffffff, 1.0 );
     scene.add( pointLight );
     pointLight.position.set(0, 100, -800);
+  }
 
+
+  function start(gifs, gifsCache) {
     const positions = setPositions(gifs.length);
     for(let i = 0; i < positions.length; i++) {
-      gifs[i].load(() => {
-        const gifcanvas = gifs[i].get_canvas();
-        // MATERIAL
-        const material = new THREE.MeshStandardMaterial({
-          color: 0xffffff
-        });
-        material.map = new THREE.Texture( gifcanvas );
-        material.displacementMap = material.map;
-        materials.push(material);
-        // GEOMETRY
-        const geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, WIDTH, HEIGHT);
-        const mesh = new THREE.Mesh( geometry, material);
-        mesh.rotation.y = Math.PI;
-        mesh.position.x = positions[i].x;
-        mesh.position.y = positions[i].y;
-        //mesh.position.z = getRandomInt(-500, -100);
-        scene.add(mesh);
+      const gifcanvas = gifs[i].get_canvas();
+      // MATERIAL
+      const material = new THREE.MeshStandardMaterial({
+        color: 0xffffff
       });
+      material.map = new THREE.Texture( gifcanvas );
+      material.displacementMap = material.map;
+      materials.push(material);
+      // GEOMETRY
+      const geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, WIDTH, HEIGHT);
+      const mesh = new THREE.Mesh( geometry, material);
+      mesh.rotation.y = Math.PI;
+      mesh.position.x = positions[i].x;
+      mesh.position.y = positions[i].y;
+      //mesh.position.z = getRandomInt(-500, -100);
+      scene.add(mesh);
     }
 
     setInterval("update()", 30);
@@ -143,6 +170,7 @@
   }
 
   function update() {
+    // todo check controls and renderer
     for(let i = 0; i < materials.length; i++) {
       materials[i].map.needsUpdate = true;
     }
@@ -155,10 +183,10 @@
   }
 
   window.onload = function() {
-    const nbImages = createImgTags();
+    createImgTags();
     const loadFunction = () => {
-      start();
-      controls.update();
+      init();
+      initScene();
     }
-    setTimeout(loadFunction, nbImages * 100);
+    setTimeout(loadFunction, 100);
   }
